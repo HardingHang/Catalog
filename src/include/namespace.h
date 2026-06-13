e0b4e3c08c0991555d112f257ff2a684632d39f8
zhengyongpan <zhengyongpan@h-partners.com>
2026-06-13 15:34:19 +0800

    implement of is_namespace_existed SQL function

---
+++ src/include/namespace.h
@@ -0,0 +1,8 @@
+#ifndef ICEBERG_CATALOG_NAMESPACE_H
+#define ICEBERG_CATALOG_NAMESPACE_H
+
+#include "fmgr.h"
+
+extern "C" Datum iceberg_is_namespace_existed(PG_FUNCTION_ARGS);
+
+#endif /* ICEBERG_CATALOG_NAMESPACE_H */
